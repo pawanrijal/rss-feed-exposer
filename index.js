fa0305bdde22d485922d92a64d6807b9c32dfcbe
Pawan Rijal <pawan.rijal@texasintl.edu.np>
2022-05-31 07:34:55 +0545

    initialized sequelize with psql

--- index.js
+++ index.js
@@ -2,6 +2,19 @@ const express = require("express");
 const app = express();
 require("dotenv").config();
 const PORT = process.env.PORT;
+const { sequelize } = require("./lib/databaseConnection");
+const bodyParser = require("body-parser");
+
+//sequelize authentication to database
+sequelize
+  .authenticate()
+  .then(() => {
+    sequelize.sync({ alter: true });
+    console.log("Database connected successfully");
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
 
 //test
 app.get("/test", (req, res) => {
